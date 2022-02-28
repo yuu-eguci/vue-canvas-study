c6e02511a2b12c5de489f25fb65fe96bea7f7911
yuu-eguci <yuu.eguci@gmail.com>
2022-02-28 17:17:00 +0900

    ✨ 文字入力を作成

--- src/components/TheStringInput.vue
+++ src/components/TheStringInput.vue
@@ -1,5 +1,13 @@
 <template>
   <v-container>
+    <v-text-field
+      solo
+      label="The string input"
+      clearable
+      type="text"
+      counter
+      maxlength="20"
+    ></v-text-field>
   </v-container>
 </template>
 
